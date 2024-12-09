0659f2a1d061e06b314dd0f7ff5da2b96f4e62e2
Rafaferreira-dev <rafaferreiradev@gmail.com>
2024-12-09 11:15:24 -0300

    Update calculadora.sh

---
+++ calculadora.sh
@@ -0,0 +1,4 @@
+Salve o arquivo na pasta desejada
+Navegue até o diretório onde o arquivo está localizado: Use o comando cd para ir até o diretório onde o seu arquivo .sh está:
+Execute o arquivo: Para executar o arquivo, utilize o comando ./ seguido o nome do arquivo.
+Ex: ./seu_arquivo.sh
